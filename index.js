03c529861ca7a5226d9818baa4b4887f5412eb84
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-10-11 14:40:22 -0400

    Expose 'init()' function as sole export from 'index.js'.

--- index.js
+++ index.js
@@ -11,17 +11,4 @@ const { init } = require( './lib/main' );
 // --------------------------------------------------
 // PUBLIC API
 // --------------------------------------------------
-/// TEMP - START
-init( {
-	entryDir: `${__dirname}/demo/public_html`,
-	viewsDir: `${__dirname}/demo/public_html/views`,
-} )
-.then( ( data ) => {
-	console.log( data );  /// TEMP
-} )
-.catch( ( err ) => {
-	console.log( err ); /// TEMP
-} );
-/// TEMP - END
-
-/// TODO[@jrmykolyn]: Expose module `init()` function.
+module.exports = init;
